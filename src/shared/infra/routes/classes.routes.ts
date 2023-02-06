f29366e67ecc1ab36308d98760f357d9be4a8d3a
theluanz <luanzuffo.lz@gmail.com>
2023-02-06 00:59:46 -0300

    FEAT: Read Class from day of week

--- src/shared/infra/routes/classes.routes.ts
+++ src/shared/infra/routes/classes.routes.ts
@@ -1,5 +1,6 @@
 import { CreateClassController } from '@modules/classes/useCases/CreateClass/CreateClassController';
 import { ReadClassController } from '@modules/classes/useCases/ReadClass/ReadClassController';
+import { ReadClassesWeekController } from '@modules/classes/useCases/ReadClassesWeek/ReadClassesWeekController';
 import { UpdateClassController } from '@modules/classes/useCases/UpdateClass/UpdateClassController';
 import { authMiddleware } from '@shared/middleares/authMiddleware';
 import { ensureIsAdminMiddleware } from '@shared/middleares/ensureIsAdminMiddleware';
@@ -8,15 +9,38 @@ import { Router } from 'express';
 const createClassController = new CreateClassController();
 const readClassController = new ReadClassController();
 const updateClassController = new UpdateClassController();
+const readClassesWeekController = new ReadClassesWeekController();
 
 const classesRoutes = Router();
 
+classesRoutes.get(
+  '/class',
+  authMiddleware,
+  ensureIsAdminMiddleware,
+  readClassesWeekController.handle,
+);
+
 classesRoutes.post('/class', authMiddleware, ensureIsAdminMiddleware, createClassController.handle);
 
-classesRoutes.get('/class/:id', authMiddleware, ensureIsAdminMiddleware, readClassController.handle);
+classesRoutes.get(
+  '/class/:id',
+  authMiddleware,
+  ensureIsAdminMiddleware,
+  readClassController.handle,
+);
 
-classesRoutes.delete('/class/:id', authMiddleware, ensureIsAdminMiddleware, readClassController.handle);
+classesRoutes.delete(
+  '/class/:id',
+  authMiddleware,
+  ensureIsAdminMiddleware,
+  readClassController.handle,
+);
 
-classesRoutes.patch('/class/:id', authMiddleware, ensureIsAdminMiddleware, updateClassController.handle);
+classesRoutes.patch(
+  '/class/:id',
+  authMiddleware,
+  ensureIsAdminMiddleware,
+  updateClassController.handle,
+);
 
 export { classesRoutes };
