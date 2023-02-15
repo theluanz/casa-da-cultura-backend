be75ddb952589253137eebfeb906329aa0037eda
theluanz <luanzuffo.lz@gmail.com>
2023-02-14 23:50:58 -0300

    FIX: Update Values on Controller

--- src/modules/students/useCases/CreateStudent/CreateStudentController.ts
+++ src/modules/students/useCases/CreateStudent/CreateStudentController.ts
@@ -3,16 +3,19 @@ import { CreateStudentUseCase } from './CreateStudentUseCase';
 
 class CreateStudentController {
   async handle(request: Request, response: Response) {
-    const { bornDate, cpf, name, period, fatherName, motherName } = request.body;
-    
+    const { bornDate, cpf, address, name, parent, period, rg, phone } = request.body;
+
     const createStudentUseCase = new CreateStudentUseCase();
 
     const newStudent = await createStudentUseCase.execute({
       bornDate,
       cpf,
+      address,
       name,
+      parent,
       period,
-      
+      rg,
+      phone,
     });
     return response.status(201).json(newStudent);
   }
